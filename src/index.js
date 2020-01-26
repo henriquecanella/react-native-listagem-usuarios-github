f383cd93d6ad90b6ac06d6d05074ab44910c326e
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-26 17:06:05 -0300

    Configurando ESLint, Prettier, EditorConfig e Reactotron

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+console.tron.log('Hello World');
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
